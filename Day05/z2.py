63e9eaa7e217e6abb720c72b6d11d9b7678a1a07
Michał Doros <dorosmichal06@gmail.com>
2022-12-06 19:22:50 +0100

    Day 05

---
+++ Day05/z2.py
@@ -0,0 +1,37 @@
+from args import INPUT_FILE
+from parse import parse
+
+def parse_stacks(stack_lines):
+    content, numbers = stack_lines[:-1], stack_lines[-1]
+    numbers = numbers.strip().split()
+    N = int(numbers[-1])
+
+    stacks = [list() for _ in range(N)]
+    for line in reversed(content):
+        for i, crate in enumerate(line.strip().replace('[','').replace(']', '').split()):
+            if crate == '-':
+                continue
+            else:
+                stacks[i].append(crate)
+    return stacks
+            
+def solution():
+    with open(INPUT_FILE, 'r') as file:
+        stack_lines = []
+        while (line := next(file)) != '\n':
+            stack_lines.append(line)
+        stacks = parse_stacks(stack_lines)
+        print(stacks)
+
+        for line in file:
+            n, src, dest = parse("move {:d} from {:d} to {:d}\n", line).fixed
+            stacks[dest-1].extend(stacks[src-1][-n:])
+            del stacks[src-1][-n:]
+
+    result = []
+    for stack in stacks:
+        result.append(stack.pop())
+
+    return "".join(result)
+
+print(solution())
