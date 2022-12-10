9417d8e7a0f189386e07650b2372293b7d071b74
Michał Doros <dorosmichal06@gmail.com>
2022-12-10 21:16:36 +0100

    Day 09

---
+++ Day09/z2.py
@@ -0,0 +1,36 @@
+from args import INPUT_FILE
+
+direction_map = {
+    'U': complex(0, 1),
+    'R': complex(1, 0),
+    'L': complex(-1, 0),
+    'D': complex(0, -1)
+}
+
+def move_closer(src, dest):
+    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    return min((src + complex(a,b) for a, b in dirs), key=lambda x: abs(dest - x))
+
+
+def solution():
+    start = complex(0,0)
+    NUMBER_OF_KNOTS = 10
+    knots = [start for _ in range(NUMBER_OF_KNOTS)]
+    visited_possitions = set()
+    visited_possitions.add(start)
+
+    with open(INPUT_FILE, 'r') as file:
+        for line in file:
+            direction, n = line.split()
+            for _ in range(int(n)):
+                knots[0] += direction_map[direction]
+                for i in range(1, NUMBER_OF_KNOTS):
+                    if abs(knots[i-1] - knots[i]) >= 2:
+                        knots[i] = move_closer(knots[i], knots[i-1])
+                visited_possitions.add(knots[NUMBER_OF_KNOTS-1])
+        
+    return len(visited_possitions)
+
+
+
+print(solution())
